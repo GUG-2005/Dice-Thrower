791bf2118b092fb2d1c843bfe5d40fc349494cb7
Gugaan <111267744+GUG-2005@users.noreply.github.com>
2024-09-30 22:44:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var randomNumber1 = Math.floor(Math.random() * 6)+1;
 var randomImg1 = document.querySelector(".img1");
-randomImg1.setAttribute("src", "./images/dice"+randomNumber1+".png");
+randomImg1.setAttribute("src", "dice"+randomNumber1+".png");
 
 var randomNumber2 = Math.floor(Math.random() * 6)+1;
 var randomImg2 = document.querySelector(".img2");
-randomImg2.setAttribute("src", "./images/dice"+randomNumber2+".png");
+randomImg2.setAttribute("src", "dice"+randomNumber2+".png");
 
 if (randomNumber1 > randomNumber2)
 {
